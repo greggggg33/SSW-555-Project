99564c56131011a76d0f2cb93e6d02f19b027a01
Guowei Li <greggggg33@gmail.com>
2020-10-29 20:04:20 -0400

    sprint 2

--- homework4 test.py
+++ homework4 test.py
@@ -1,20 +1,20 @@
 import unittest
-from homework4 import Gedcom
+from homework4 import searchByYears, getID
 
+#tests for searching how many people were born in a given time frame.
 #tests for searching how many people were born in a given time frame.
 class BuggyGedcom(unittest.TestCase):
     def test_searchByYear(self):
-        "test if obeject stored properly in __init__()"
-        f = Gedcom("test GEDCOM file",1960,1970)
-        self.assertEqual(f.searchByYears(),2)
-        f = Gedcom("test GEDCOM file",1950,1960)
-        self.assertEqual(f.searchByYears(),3)
-        f = Gedcom("test GEDCOM file",1950,2000)
-        self.assertEqual(f.searchByYears(),10)
-        f = Gedcom("test GEDCOM file",1980,2000)
-        self.assertEqual(f.searchByYears(),7)
-        f = Gedcom("test GEDCOM file",1900,2020)
-        self.assertEqual(f.searchByYears(),11)
+        #test 1
+        self.assertEqual(searchByYears("test GEDCOM file",1950,1970),3)
+        #test 2
+        self.assertEqual(searchByYears("test GEDCOM file",1900,2000),10)
+        #test 3
+        self.assertEqual(searchByYears("test GEDCOM file",1970,2000),7)
+    
+    def test_getId(self):
+        #test 1
+        self.assertEqual(getID("test GEDCOM file"), {'Nancy': 'I01', 'Jack': 'I02', 'Mike': 'I03', 'Greg': 'I07', 'Emma': 'I05', 'STEVE': 'I06'})
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit = False)
--- homework4.py
+++ homework4.py
@@ -1,43 +1,49 @@
+#Guowei Li
+def printGED(text_file):
+    f = open(text_file+'.txt',"r")
+    for lines in f:
+        temp = lines.split(" ",2)
+        temp[-1] = temp[-1][:-1]
+        print ("--> " + lines, end ='')
+        if temp[1] in ("NOTE","NAME","SEX","FAMC","FAMS","HUSB","WIFE","CHIL","DATE"):
+            print ("<-- {}|{}|Y|{}".format(temp[0],temp[1],temp[2]))
+        elif temp[1] in ("BIRT","DEAT","MARR","DIV"):
+            print ("<-- {}|{}|Y".format(temp[0],temp[1]))
+        elif temp[2] in ("INDI","FAM"):
+            print ("<-- {}|{}|Y|{}".format(temp[0],temp[2],temp[1]))
+        else:
+            print ("<-- {}|{}|N|{}".format(temp[0],temp[1],temp[2]))
+    return None
 
-class Gedcom():
-    def __init__(self, filename, startYear,endYear):
-        self.filename = filename + ".txt"
-        self.startYear = startYear
-        self.endYear = endYear
-    def print(self):
-        f = open(self.filename,"r")
-        for lines in f:
-            temp = lines.split(" ",2)
-            temp[-1] = temp[-1][:-1]
-            print ("--> " + lines, end = '')
-            if temp[1] in ("NOTE","NAME","SEX","FAMC","FAMS","HUSB","WIFE","CHIL","DATE"):
-                print ("<-- {}|{}|Y|{}".format(temp[0],temp[1],temp[2]))
-            elif temp[1] in ("BIRT","DEAT","MARR","DIV"):
-                print ("<-- {}|{}|Y".format(temp[0],temp[1]))
-            elif temp[2] in ("INDI","FAM"):
-                print ("<-- {}|{}|Y|{}".format(temp[0],temp[2],temp[1]))
-            else:
-                print ("<-- {}|{}|N|{}".format(temp[0],temp[1],temp[2]))
-        return None
-    #first sprint for searching people were born in a given time frame
-    def searchByYears(self):
-        "The method shows how many people was born between the startyear and endyear"
-        f = open(self.filename,'r')
-        result = 0
-        for lines in f:
-            temp = lines.split(" ")
-            temp[-1] = temp[-1][:-1]
-            #print (temp)
-            if temp[1] == "DATE":
-                if int(temp[4]) >= self.startYear and int(temp[4]) <= self.endYear:
-                    print("I have fund someone was born {} {} {}".format(temp[2],temp[3],temp[4]))
-                    result += 1
-        print("there are {} people was borned from {} to {}".format(result,self.startYear,self.endYear))
-        return result
+def searchByYears(text_file, startYear, endYear):
+    "The method shows how many people was born between the startyear and endyear"
+    f = open(text_file+".txt",'r')
+    result = 0
+    for lines in f:
+        temp = lines.split(" ")
+        temp[-1] = temp[-1][:-1]
+        if temp[1] == "DATE":
+            if int(temp[4]) >= startYear and int(temp[4]) <= endYear:
+                print("I have fund someone was born {} {} {}".format(temp[2],temp[3],temp[4]))
+                result += 1
+    print("there are {} people was borned from {} to {}".format(result,startYear,endYear))
+    return result
+
+def getID(text_file): #return a dictionary that store everyone's name and their ID number in the GED#
+    f = open(text_file+".txt",'r')
+    lines = f.read().splitlines()
+    result = {}
+    c = 0 #counter to iterate the list
+    while c < len(lines):
+        temp = lines[c].split(" ")
+        if temp[1].startswith("I"):
+            result[lines[c+1].split(" ")[2]] = temp[1] #set the key as the name of the individual and value as its ID number
+        c += 1
+    print(result)
+    return result
 
 def main():
-    f = Gedcom("test GEDCOM file",1950,1960)
-    f.searchByYears()
+    printGED("test GEDCOM file")
 
 if __name__ == "__main__":
     main()
